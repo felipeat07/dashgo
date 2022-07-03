afc9665a5ad00cc6a8dd1b5f674f10d896c1aea8
Felipe Tavares <felipeat07@gmail.com>
2022-07-03 22:21:03 +0100

    finishing sign page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,71 @@
+import { Button, Flex, FormLabel, Input, Stack, FormControl } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <h1>Dashgo</h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      alignItems="center"
+      justifyContent="center"
+    >
+
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        background="gray.800"
+        padding="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+
+        <Stack spacing="4">
+
+          <FormControl>
+
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              name="email"
+              type="email" 
+              id="email"
+              focusBorderColor="pink.500"
+              backgroundColor="gray.900"
+              variant="filled"
+              _hover={{
+                backgroundColor: 'gray.900'
+              }}
+              size="lg"
+            />
+
+          </FormControl>
+
+          <FormControl>
+
+            <FormLabel htmlFor="password">Senha</FormLabel>
+            <Input
+              name="password"
+              type="password"
+              id="password" 
+              focusBorderColor="pink.500"
+              backgroundColor="gray.900"
+              variant="filled"
+              _hover={{
+                backgroundColor: 'gray.900'
+              }}
+              size="lg"
+            />
+
+          </FormControl>
+
+        </Stack>
+
+        <Button type="submit" marginTop="6" colorScheme="pink" size="lg">Entrar</Button>
+
+      </Flex>
+
+
+    </Flex>
   )
 }
 
