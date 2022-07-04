50ba55021b5df9f6f71563c002e5714bf9406fc4
Felipe Tavares <felipeat07@gmail.com>
2022-07-04 22:48:42 +0100

    create a static sidebar component

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,16 @@
+import { Flex } from "@chakra-ui/react";
+import { Sidebar } from "../components/Form/Sidebar";
 import { Header } from "../components/Header";
 
 export default function Dashboard(){
-    return <Header />
+    return (
+        <Flex direction="column" height="100vh">
+            <Header />
+
+            <Flex width="100%" marginY="6" maxWidth={1480} marginX="auto" paddingX="6">
+                <Sidebar />
+            </Flex>
+
+        </Flex>
+    )
 }
