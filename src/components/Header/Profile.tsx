b214055d2ed0bdc05fe73432c6f41f7e0db33d14
Felipe Tavares <felipeat07@gmail.com>
2022-07-12 22:52:36 +0100

    responsive chakra style

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,12 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true}: ProfileProps) {
     return (
         <Flex alignItems="center">
-            <Box mr="4" textAlign="right" >
+            
+            {showProfileData && (
+                <Box mr="4" textAlign="right" >
                 <Text>Felipe Tavares</Text>
                 <Text color="gray.300" fontSize="small">felipeat07@gmail.com</Text>
             </Box>
+            )}
 
             <Avatar size="md" name="Felipe Tavares" src="https://www.github.com/felipeat07.png" />
         </Flex>
