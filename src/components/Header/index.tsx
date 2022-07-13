e6aedb0a218e8618bbb6bbc7e934b62ab02480fa
Felipe Tavares <felipeat07@gmail.com>
2022-07-13 16:17:28 +0100

    create a responsive sidebar with chakraUI

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,15 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react'
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react'
+import { RiMenuLine } from 'react-icons/ri'
+import { useSidebarDrawer } from '../../context/SidebarDrawerContext'
 import { Logo } from './Logo'
 import { NotificationNav } from './NotificationNav'
 import { Profile } from './Profile'
 import { SearchBox } from './SearchBox'
 
 export function Header() {
+    const { onOpen } = useSidebarDrawer()
+    
+
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true
@@ -22,6 +27,20 @@ export function Header() {
             paddingX="6"
             alignItems="center"
         >
+            { !isWideVersion && (
+                <IconButton
+                    aria-label="Open navigation"
+                    icon={<Icon as={RiMenuLine} />}
+                    fontSize="24"
+                    variant="unstyled"
+                    onClick={onOpen}
+                    mr="2"
+                >
+
+                </IconButton>
+            )}
+
+
             <Logo />
 
             {isWideVersion && <SearchBox />}
